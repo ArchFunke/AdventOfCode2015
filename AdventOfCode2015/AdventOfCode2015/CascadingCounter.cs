33d41c6c419f0e2b0d54c011e461e53cf73ff8f6
ArchFunke <michael.falter5@gmail.com>
2023-05-11 20:57:33 -0400

    Completed Day 1
    Created solution and added generic method useful for day 1.

---
+++ AdventOfCode2015/AdventOfCode2015/CascadingCounter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace AdventOfCode2015
+{
+    public static class CascadingCounter<T>
+    {
+        /// <summary>
+        /// Takes array of two values where one value increases counter and one value decreases counter.
+        /// Returns counter after all items in array are considered.
+        /// </summary>
+        /// <remarks>Useful for figuring out net force applied in a direction. Used for day 1, and modified for day 2.</remarks>
+        public static int CascadeCount<T>(T incrementValue, T decrementValue, T[] sourceArray)
+        {
+            int counter = 0;
+            foreach (T value in sourceArray)
+            {
+                if (value.Equals(incrementValue))
+                {
+                    counter++;
+                }
+                else if (value.Equals(decrementValue))
+                {
+                    counter--;
+                }
+            }
+            return counter;
+        }
+    }
+}
---
+++ AdventOfCode2015/AdventOfCode2015/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using AdventOfCode2015;
+using System.Collections.Generic;
+
+namespace AdventOfCode2015
+{
+    internal class Program
+    {
+        // Will call general methods depending on the problem
+        static void Main(string[] args)
+        {
+            try
+            {
+                string input;
+                using (var streamReader = new StreamReader(@"C:\Users\Michael\Documents\Development\AdventOfCode2015\AdventOfCode2015\Input\day1Input.txt")) 
+                {
+                    input = streamReader.ReadToEnd();
+                }
+                Console.WriteLine(CascadingCounter<char>.CascadeCount<char>('(', ')', input.ToArray()));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
