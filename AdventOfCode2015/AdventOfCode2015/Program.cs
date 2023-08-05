0c673543a2b865b13ba34b26fab58084d4d0648c
Michael Falter <evileyefive@gmail.com>
2023-08-05 19:43:11 -0400

    Added initial read for day2 and useful data storage.

--- AdventOfCode2015/AdventOfCode2015/Program.cs
+++ AdventOfCode2015/AdventOfCode2015/Program.cs
@@ -13,17 +13,43 @@ namespace AdventOfCode2015
         {
             try
             {
-                string input;
-                using (var streamReader = new StreamReader("../../../Input/day1Input.txt")) 
-                {
-                    input = streamReader.ReadToEnd();
-                }
-                Console.WriteLine(CascadingCounter<char>.CascadeCount<char>('(', ')', input.ToArray()));
+                Day2();
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
             }
+        }
+
+        static private void Day2()
+        {
+            List<IList<int>> dimensionList = new List<IList<int>>();
+            using (var streamReader = new StreamReader("../../../Input/day2Input.txt"))
+            {
+                string entry;
+                while ((entry = streamReader.ReadLine()) != null)
+                {
+                    var split = entry.Split('x');
+                    List<int> curLine = new List<int>();
+                    foreach (var num in split)
+                    {
+                        curLine.Add(Int32.Parse(num));
+                    }
+                    curLine.Sort();
+                    dimensionList.Add(curLine);
+                }
+            }
+            Console.WriteLine("ToDo");
+            // Have list of dimensions. Get total.
+        }
+
+        static private void Day1(string input)
+        {
+            using (var streamReader = new StreamReader("../../../Input/day1Input.txt"))
+            {
+                input = streamReader.ReadToEnd();
+            }
+            Console.WriteLine(CascadingCounter<char>.CascadeCount<char>('(', ')', input.ToArray()));
         }
     }
 }
